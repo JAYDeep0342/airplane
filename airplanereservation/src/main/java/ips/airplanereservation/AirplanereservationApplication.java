5064552fcee22d1cc8ac676ad17ccc774d1a0910
Jaydeep0342 <jaydeep0342@gmail.com>
2026-01-24 19:04:29 +0530

    Initial commit - JWT auth with DTO

---
+++ airplanereservation/src/main/java/ips/airplanereservation/AirplanereservationApplication.java
@@ -0,0 +1,13 @@
+package ips.airplanereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplanereservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplanereservationApplication.class, args);
+	}
+
+}
---
+++ airplanereservation/src/test/java/ips/airplanereservation/AirplanereservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ips.airplanereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirplanereservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
